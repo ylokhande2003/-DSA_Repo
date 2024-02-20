1514bdb8774a20b7cf9ca538a22348c8624419d0
Yash lokhande <91784682+ylokhande2003@users.noreply.github.com>
2024-02-20 17:22:15 +0530

    Time: 64 ms (48.04%), Space: 51.2 MB (36.98%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    nums.sort((a,b)=>(a-b));
+    for(var i=0;i<nums.length;i++)
+        {
+            if(nums[i]!==i){
+                return i;
+            }
+        }
+    return i;
+};
